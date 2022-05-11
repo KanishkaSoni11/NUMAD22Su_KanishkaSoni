e56e32c0f9dbe35fc923e5dc6f9f54a20ef27134
Kanishka Soni <kanishkasoni@Kanishkas-MacBook-Pro.local>
2022-05-10 23:52:26 -0400

    button added

--- app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/MainActivity.java
@@ -2,13 +2,28 @@ package edu.neu.madcourse.numad22su_kanishkasoni;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickAbout_me(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
     }
 }
