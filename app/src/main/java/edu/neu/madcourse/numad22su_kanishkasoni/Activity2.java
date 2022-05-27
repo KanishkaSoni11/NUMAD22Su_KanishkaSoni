e39d136a7da55d6859f4a37dcf18dffc4f621a2f
Kanishka Soni <kanishkasoni@Kanishkas-MacBook-Pro.local>
2022-05-27 01:23:30 -0400

    Added on click for all buttons

--- app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity2.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -13,7 +14,28 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
     }
 
-    public void onClick_Act2(View view){
+    public void onClick_Act2(View view) {
+        TextView textView = (TextView) findViewById(R.id.pressed_text);
+
+        if (view.getId() == R.id.Button_A) {
+            textView.setText("Pressed : A");
+        } else if (view.getId() == R.id.Button_B) {
+            textView.setText("Pressed : B");
+        }
+        else if (view.getId() == R.id.Button_C) {
+            textView.setText("Pressed : C");
+        }
+        else if (view.getId() == R.id.Button_D) {
+            textView.setText("Pressed : D");
+        }
+        else if (view.getId() == R.id.Button_E) {
+            textView.setText("Pressed : E");
+        }
+        else if (view.getId() == R.id.Button_F) {
+            textView.setText("Pressed : F");
+        }
+
+
 
     }
 }
