294dea5ff9154cb5a249fdfba6ffdbc49669a377
Kanishka Soni <kanishkasoni@Kanishkas-MacBook-Pro.local>
2022-05-27 00:31:23 -0400

    added new buttons

--- app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity2.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity2.java
@@ -3,6 +3,7 @@ package edu.neu.madcourse.numad22su_kanishkasoni;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+
+    public void onClick_Act2(View view){
+
     }
 }
