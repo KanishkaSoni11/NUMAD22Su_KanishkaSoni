84aeb99a0e09bd1df3cdc13bce414aed6fb44dcf
Kanishka Soni <kanishkasoni@Kanishkas-MacBook-Pro.local>
2022-06-11 13:47:18 -0500

    Added prime func

--- app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity_Thread.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity_Thread.java
@@ -2,13 +2,68 @@ package edu.neu.madcourse.numad22su_kanishkasoni;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Activity_Thread extends AppCompatActivity {
+    private static final String TAG = "Activity_Thread";
+    private Handler textHandler = new Handler();
+    TextView textView_number;
+    TextView textView_numberBeingChecked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thread);
+        textView_number = findViewById(R.id.textView_number);
+        textView_numberBeingChecked = findViewById(R.id.textView_numberChecked);
+    }
+
+    public void onClick_findPrimes(View view) {
+        runnableThread runnableThread = new runnableThread();
+        new Thread(runnableThread).start();
+
+    }
+
+    class runnableThread implements Runnable {
+        boolean isPrime(int n) {
+            if (n == 2)
+                return true;
+            else if (n % 2 == 0)
+                return false;
+
+            for (int i = 3; i <= Math.sqrt(n); i += 2) {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        @Override
+        public void run() {
+
+            for (int i = 2; i <= 10; i++) {
+                final int finalI = i;
+
+                //The handler changes the TextView running in the UI thread.
+                textHandler.post(() -> {
+                    textView_numberBeingChecked.setText("Number being Checked: " + finalI);
+                    if (isPrime(finalI)) {
+                        textView_number.setText("Prime Number: " + finalI );
+                    }
+                });
+                try {
+                    Thread.sleep(1000); //Makes the thread sleep or be inactive for 10 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
