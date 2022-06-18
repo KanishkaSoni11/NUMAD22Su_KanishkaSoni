e6a1e16f44b0aba39a8bb68cf900d0249eafa2ea
Kanishka Soni <kanishkasoni@Kanishkas-MacBook-Pro.local>
2022-06-18 16:46:45 -0500

    location manager

--- app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity_Location.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity_Location.java
@@ -1,14 +1,67 @@
 package edu.neu.madcourse.numad22su_kanishkasoni;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class Activity_Location extends AppCompatActivity {
+public class Activity_Location extends AppCompatActivity implements LocationListener {
+    private TextView textViewLatitude;
+    private TextView textViewLongitude;
+    private LocationManager locationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        textViewLatitude = (TextView) findViewById(R.id.textView_latitude);
+        textViewLongitude = (TextView) findViewById(R.id.textView_longitude);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+        onLocationChanged(location);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        textViewLatitude.setText("" + latitude);
+        textViewLongitude.setText("" + longitude);
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
     }
 }
