3899c3a6f40e19961d70078bf2420dc1a574e527
Kanishka Soni <kanishkasoni@Kanishkas-MacBook-Pro.local>
2022-06-19 18:04:46 -0500

    Final apk

--- app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity_Location.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity_Location.java
@@ -6,13 +6,11 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.ResolvableApiException;
+
 import com.google.android.gms.location.LocationRequest;
 
 import android.os.Bundle;
@@ -25,12 +23,7 @@ import android.widget.Toast;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.Priority;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 public class Activity_Location extends AppCompatActivity {
     private static final int REQUEST_LOCATION = 1;
@@ -71,7 +64,7 @@ public class Activity_Location extends AppCompatActivity {
         textViewDistance = findViewById(R.id.textView_Distance);
 
         locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
 
         locationRequest.setInterval(500);
         locationRequest.setFastestInterval(100);
@@ -191,41 +184,7 @@ public class Activity_Location extends AppCompatActivity {
     }
 
     private void turnOnGPS() {
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-        builder.setAlwaysShow(true);
-
-        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
-                .checkLocationSettings(builder.build());
-
-        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
-            @Override
-            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
-
-                try {
-                    LocationSettingsResponse response = task.getResult(ApiException.class);
-                    Toast.makeText(Activity_Location.this, "GPS is already turned on", Toast.LENGTH_SHORT).show();
-
-                } catch (ApiException e) {
-
-                    switch (e.getStatusCode()) {
-                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-
-                            try {
-                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
-                                resolvableApiException.startResolutionForResult(Activity_Location.this, 2);
-                            } catch (IntentSender.SendIntentException ex) {
-                                ex.printStackTrace();
-                            }
-                            break;
-
-                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                            break;
-                    }
-                }
-            }
-        });
-
+        Toast.makeText(this, "Please turn o GPS", Toast.LENGTH_SHORT).show();
     }
 
 
@@ -243,7 +202,7 @@ public class Activity_Location extends AppCompatActivity {
 
     public void onClick_Approx() {
         locationRequest = LocationRequest.create();
-        locationRequest.setPriority(Priority.PRIORITY_LOW_POWER);
+        locationRequest.setPriority(Priority.PRIORITY_BALANCED_POWER_ACCURACY);
 
         locationRequest.setInterval(1000);
         locationRequest.setFastestInterval(1000);
