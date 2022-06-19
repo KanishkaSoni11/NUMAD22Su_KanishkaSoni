1b41641672b46079718299f0e89fb7ea66c8ea56
Kanishka Soni <kanishkasoni@Kanishkas-MacBook-Pro.local>
2022-06-19 00:41:55 -0500

    location

--- app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity_Location.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_kanishkasoni/Activity_Location.java
@@ -6,21 +6,40 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Activity_Location extends AppCompatActivity implements LocationListener {
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class Activity_Location extends AppCompatActivity {
     private static final int REQUEST_LOCATION = 1;
     private TextView textViewLatitude;
     private TextView textViewLongitude;
-    private LocationManager locationManager;
+    private LocationRequest locationRequest;
     private TextView textViewDistance;
     private int distance;
+    private double startLatitute;
+    private double startLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,55 +51,124 @@ public class Activity_Location extends AppCompatActivity implements LocationList
 
         textViewLatitude = (TextView) findViewById(R.id.textView_latitude);
         textViewLongitude = (TextView) findViewById(R.id.textView_longitude);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
         textViewDistance = findViewById(R.id.textView_Distance);
 
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]
-                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
+
+        getCurrentLocation();
+
+    }
+
+
+    public void getCurrentLocation() {
+
+        if (ActivityCompat.checkSelfPermission(Activity_Location.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            if (isGPSEnabled()) {
+
+                LocationServices.getFusedLocationProviderClient(Activity_Location.this)
+                        .requestLocationUpdates(locationRequest, new LocationCallback() {
+                            @Override
+                            public void onLocationResult(@NonNull LocationResult locationResult) {
+                                super.onLocationResult(locationResult);
+
+                                LocationServices.getFusedLocationProviderClient(Activity_Location.this);
+
+
+                                if (locationResult.getLocations().size() > 0) {
+
+                                    int index = locationResult.getLocations().size() - 1;
+
+                                    double endLatitude = locationResult.getLocations().get(index).getLatitude();
+                                    double endLongitude = locationResult.getLocations().get(index).getLongitude();
+                                    float[] results = new float[1];
+
+                                    textViewLatitude.setText("Latitude : " + endLatitude);
+                                    textViewLongitude.setText("Longitude: " + endLongitude);
+
+                                    Location.distanceBetween(endLatitude, endLongitude, endLatitude, endLongitude, results);
+
+                                    textViewDistance.setText("Distance : " + (distance + results[0]));
+                                }
+                            }
+                        }, Looper.getMainLooper());
+
+            } else {
+                turnOnGPS();
+            }
+
+        } else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+    }
+
+
+//        Location.distanceBetween(latitude, longitude, latitude, longitude, results);
+//        distance = 2;
+//        textViewDistance.setText("Distance : " + (distance+ results[0]));
+
+
+    private boolean isGPSEnabled() {
+        LocationManager locationManager = null;
+        boolean isEnabled = false;
+
+        if (locationManager == null) {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         }
 
-
-        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
-
-        onLocationChanged(location);
-    }
-
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-        float[] results = new float[1];
-        double longitude = location.getLongitude();
-        double latitude = location.getLatitude();
-        textViewLatitude.setText("Latitude : " + latitude);
-        textViewLongitude.setText("Longitude: " + longitude);
-
-
-        Location.distanceBetween(latitude, longitude, latitude, longitude, results);
-        distance = 2;
-        textViewDistance.setText("Distance : " + (distance+ results[0]));
+        isEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return isEnabled;
 
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        LocationListener.super.onStatusChanged(provider, status, extras);
-    }
-
-    @Override
-    public void onProviderEnabled(@NonNull String provider) {
-        LocationListener.super.onProviderEnabled(provider);
-    }
-
-    @Override
-    public void onProviderDisabled(@NonNull String provider) {
-        LocationListener.super.onProviderDisabled(provider);
-    }
 
     public void onClick_ResetDistance(View view) {
         distance = 0;
         textViewDistance.setText("Distance : " + 0);
+    }
+
+    private void turnOnGPS() {
+
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(Activity_Location.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(Activity_Location.this, 2);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        });
+
     }
 }
